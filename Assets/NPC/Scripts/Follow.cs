895186a9ab60292b40ae7f33ac9f8e3c0275b248
Updesh Pr. Singh <updesh071@gmail.com>
2023-01-04 14:27:47 +0530

    update

---
+++ Assets/NPC/Scripts/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follow : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 2;
+    float distanceTravelled;
+    private void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+    }
+}
