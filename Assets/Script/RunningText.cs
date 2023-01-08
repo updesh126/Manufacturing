b1d2ffb0f12d6dea8ba6531b67b72fa7f9f9a3a2
vanshika1604 <91271424+vanshika1604@users.noreply.github.com>
2023-01-08 14:12:17 +0530

    running text

---
+++ Assets/Script/RunningText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RunningText : MonoBehaviour
+{
+    [Header("Text Settings")]
+    [SerializeField] [TextArea] private string[] itemInfo;
+    [SerializeField] private float textSpeed = 0.01f;
+
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI itemInfoText;
+    private int currentDisplayingText = 0;
+
+    public void ActivateText()
+    {
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        for (int i = 0; i < itemInfo[currentDisplayingText].Length + 1; i++)
+        {
+            itemInfoText.text = itemInfo[currentDisplayingText].Substring(0, i);
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+}
