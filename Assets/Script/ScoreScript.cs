dba282810c223ffc3cd54236ef8f36e8266896e1
Updesh Pr. Singh <updesh071@gmail.com>
2023-01-11 03:26:25 +0530

    update score star

--- Assets/Script/ScoreScript.cs
+++ Assets/Script/ScoreScript.cs
@@ -6,6 +6,13 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
+    public Image image;
+    public Sprite starSprite0;
+    public Sprite StarSprite1;
+    public Sprite StarSprite2;
+    public Sprite StarSprite3;
+
+
     public static int scoreValue = 0;
     Text score;
     // Start is called before the first frame update
@@ -22,9 +29,30 @@ public class ScoreScript : MonoBehaviour
     public void ADDScore()
     {
         scoreValue += 10;
+        CheckScore();
     }
     public void SubScore()
     {
         scoreValue -= 10;
+        CheckScore();
+    }
+    private void CheckScore()
+    {
+        if (scoreValue <= 0)
+        {
+            image.sprite = starSprite0;
+        }
+        else if (0<=scoreValue && scoreValue <= 20)
+        {
+            image.sprite = StarSprite1;
+        }
+        else if (20 <= scoreValue && scoreValue <= 50)
+        {
+            image.sprite = StarSprite2;
+        }
+        else
+        {
+            image.sprite = StarSprite3;
+        }
     }
 }
