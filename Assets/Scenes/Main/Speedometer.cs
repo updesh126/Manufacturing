d4da26f19f5f33e0a642380bf3c0210db0883bd8
Updesh Pr. Singh <updesh071@gmail.com>
2023-01-14 17:29:55 +0530

    update

---
+++ Assets/Scenes/Main/Speedometer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class Speedometer : MonoBehaviour
+{
+    private void Start()
+    {
+        Speed();
+    }
+    public static void Speed()
+    {
+        double speed = 0; // initialize speed to 0
+
+        while (true)
+        {
+            // simulate increasing speed over time
+            speed += 5;
+            Console.WriteLine("Current speed: " + speed + " mph");
+            Thread.Sleep(1000); // wait for 1 second
+        }
+    }
+}
